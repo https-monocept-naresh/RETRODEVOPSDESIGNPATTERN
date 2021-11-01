ec8e710f1f4330b21884031616c3bc8992e44679
SS110927 <sidhartha@monocept.com>
2021-11-01 19:30:22 +0530

    sucess

---
+++ singleton/SingletonExample.java
@@ -0,0 +1,15 @@
+package com.techprimers.designpatterns.singleton;
+
+public class SingletonExample {
+
+    public static void main(String[] args) {
+
+        Singleton instance = Singleton.getInstance();
+        System.out.println(instance);
+        Singleton secondInstance = Singleton.getInstance();
+        System.out.println(secondInstance);
+
+        
+
+    }
+}
